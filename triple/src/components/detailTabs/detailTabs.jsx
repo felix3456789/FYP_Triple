9ae677327a6bd1becf95faf5886665066f26daf0
kaman0916 <47221374+kaman0916@users.noreply.github.com>
2019-05-14 23:41:50 +0800

    Update detailTabs.jsx

--- triple/src/components/detailTabs/detailTabs.jsx
+++ triple/src/components/detailTabs/detailTabs.jsx
@@ -11,38 +11,38 @@ class DetailTabs extends Component {
             this.Tabs = Tabs;
           }}
           id="tabs-swipe-demo"
-          class="tabs"
+          className="tabs"
         >
-          <li class="tab col s2 ">
+          <li className="tab col s2 ">
             <a href="#test-swipe-1">本團特色</a>
           </li>
-          <li class="tab col s2">
+          <li className="tab col s2">
             <a href="#test-swipe-2">行程安排</a>
           </li>
-          <li class="tab col s2">
+          <li className="tab col s2">
             <a href="#test-swipe-3">交通資料</a>
           </li>
-          <li class="tab col s2">
+          <li className="tab col s2">
             <a href="#test-swipe-4">自費活動</a>
           </li>
-          <li class="tab col s2">
+          <li className="tab col s2">
             <a href="#test-swipe-5">注意事項</a>
           </li>
         </ul>
 
-        <div id="test-swipe-1" class="col s12 light-blue lighten-5">
+        <div id="test-swipe-1" className="col s12 light-blue lighten-5">
           Test 1
         </div>
-        <div id="test-swipe-2" class="col s12 light-blue lighten-4">
+        <div id="test-swipe-2" className="col s12 light-blue lighten-4">
           Test 2
         </div>
-        <div id="test-swipe-3" class="col s12 light-blue lighten-3">
+        <div id="test-swipe-3" className="col s12 light-blue lighten-3">
           Test 3
         </div>
-        <div id="test-swipe-4" class="col s12 light-blue lighten-2">
+        <div id="test-swipe-4" className="col s12 light-blue lighten-2">
           Test 4
         </div>
-        <div id="test-swipe-5" class="col s12 light-blue lighten-1">
+        <div id="test-swipe-5" className="col s12 light-blue lighten-1">
           Test 5
         </div>
       </React.Fragment>
