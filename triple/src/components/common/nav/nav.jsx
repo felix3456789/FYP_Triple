5a6aaa3a36cd988791d601cb2ff903d2acaba752
Felix <thc950303@gmail.com>
2019-05-15 16:31:57 +0800

    enquiryBox homepage

---
+++ triple/src/components/common/nav/nav.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+class NavBar extends Component {
+  state = {};
+  render() {
+    const { color, id } = this.props;
+    return (
+      <nav id={id}>
+        <div className={"nav-wrapper  " + color}>
+          <a href="/test" className="brand-logo">
+            Triple
+          </a>
+          <ul id="nav-mobile" className="hide-on-med-and-down loginNav--ul">
+            <li>
+              <a href="sass.html">旅遊資訊</a>
+            </li>
+            <li>
+              <a href="badges.html">獨立報團</a>
+            </li>
+            <li>
+              <a href="collapsible.html">加入我們</a>
+            </li>
+            <li>
+              <a href="collapsible.html">關於我們</a>
+            </li>
+            <li>
+              <a href="collapsible.html">聯絡我們</a>
+            </li>
+            <li>
+              <a href="collapsible.html">公司資料</a>
+            </li>
+          </ul>
+          <ul id="nav-mobile" className="right hide-on-med-and-down ">
+            <li>
+              <a href="/test">
+                <img alt="search" src="/icon/search.svg" />
+              </a>
+            </li>
+            <li>
+              <a href="badges.html">
+                <img alt="bell" src="/icon/bell.svg" />
+              </a>
+            </li>
+            <li>
+              <a href="collapsible.html">
+                <img alt="account" src="/icon/account.svg" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
--- triple/src/page/login.jsx
+++ triple/src/page/login.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import LoginBox from "../components/loginPage/loginBox/loginBox";
-import LoginNav from "../components/loginPage/nav/nav";
+import LoginNav from "../components/common/nav/nav";
 import "../css/login.css";
 class Login extends Component {
   render() {
     return (
       <div className="login__background--img">
-        <LoginNav />
+        <LoginNav
+          color="grey lighten-5 loginNav__textColor"
+          textColor="loginNav--ul"
+        />
         <div className="loginBox__layout">
           <LoginBox />
         </div>
