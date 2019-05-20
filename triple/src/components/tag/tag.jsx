880ea3c373ae2df3f4cc35f21a13ae3af14da88c
kaman0916 <47221374+kaman0916@users.noreply.github.com>
2019-05-21 07:09:26 +0800

    Update tag.jsx

--- triple/src/components/tag/tag.jsx
+++ triple/src/components/tag/tag.jsx
@@ -41,7 +41,7 @@ class Tag extends Component {
     return (
       <React.Fragment>
         <div
-          class="chips"
+          className="chips"
           id="test"
           ref={chips => {
             this.chips = chips;
@@ -50,34 +50,34 @@ class Tag extends Component {
           {this.state.tag.map(item => (
             <h1>hi</h1>
           ))}
-          <input class="Tag" />
+          <input className="Tag" />
         </div>
-        <div class="card">
-          <div class="chip" onClick={() => this.handleClick("澳門")}>
+        <div className="card">
+          <div className="chip" onClick={() => this.handleClick("澳門")}>
             澳門
           </div>
-          <div class="chip">日本</div>
-          <div class="chip">深度遊</div>
-          <div class="chip">美景遊</div>
-          <div class="chip">陽光與海灘</div>
-          <div class="chip">三日兩夜</div>
-          <div class="chip">無敵大海景</div>
-          <div class="chip">環島遊</div>
-          <div class="chip">吃喝玩樂</div>
-          <div class="chip">泰國</div>
-          <div class="chip">七天團</div>
-          <div class="chip">打卡勝地</div>
+          <div className="chip">日本</div>
+          <div className="chip">深度遊</div>
+          <div className="chip">美景遊</div>
+          <div className="chip">陽光與海灘</div>
+          <div className="chip">三日兩夜</div>
+          <div className="chip">無敵大海景</div>
+          <div className="chip">環島遊</div>
+          <div className="chip">吃喝玩樂</div>
+          <div className="chip">泰國</div>
+          <div className="chip">七天團</div>
+          <div className="chip">打卡勝地</div>
           <br />
-          <div class="chip">海鮮餐</div>
-          <div class="chip">中山</div>
-          <div class="chip">輕輕鬆鬆</div>
-          <div class="chip">首爾今期流行</div>
-          <div class="chip">今期流行</div>
-          <div class="chip">賞櫻花</div>
-          <div class="chip">富貴玩樂團</div>
-          <div class="chip">文青系</div>
-          <div class="chip">北韓深度遊</div>
-          <div class="chip">一日遊</div>
+          <div className="chip">海鮮餐</div>
+          <div className="chip">中山</div>
+          <div className="chip">輕輕鬆鬆</div>
+          <div className="chip">首爾今期流行</div>
+          <div className="chip">今期流行</div>
+          <div className="chip">賞櫻花</div>
+          <div className="chip">富貴玩樂團</div>
+          <div className="chip">文青系</div>
+          <div className="chip">北韓深度遊</div>
+          <div className="chip">一日遊</div>
         </div>
       </React.Fragment>
     );
